a513730465a4aebc43602dca9078426a054840b6
yangboy91 <yangtianbo1991@gmail.com>
2026-04-12 10:08:06 +0800

    feat: redirect www to apex domain

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,21 @@ const nextConfig: NextConfig = {
     process.env.VERCEL === "1"
       ? undefined
       : path.join(process.cwd(), "../.."),
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.finotech.xyz",
+          },
+        ],
+        destination: "https://finotech.xyz/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
